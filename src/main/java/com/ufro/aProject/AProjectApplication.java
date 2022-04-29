012f7cbcb472f61db02bee6a8e8ac8ce08c1c5c7
Amilkytool <cardemilcar@gmail.com>
2022-04-29 19:29:53 -0400

    Spring Initializer
    Inicialización de Spring Boot

---
+++ src/main/java/com/ufro/aProject/AProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ufro.aProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ufro/aProject/AProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufro.aProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
