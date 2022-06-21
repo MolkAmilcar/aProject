6468083823651c562fa6492b9c61eba99bfe16ee
isialbayay <i.albayay01@ufromail.cl>
2022-06-21 02:01:40 -0400

    corrección nombres Tests

--- src/test/java/com/ufro/aProject/JPAUnitTest.java
+++ src/test/java/com/ufro/aProject/JPAUnitTest.java
@@ -40,56 +40,56 @@ public class JPAUnitTest {
 
 
     @Test
-    public void comentariosItemIsNotEmpty(){
+    public void comentariosItemIsNotEmptyTest(){
         Iterable comentarios = comentarioItemRepository.findAll();
         assertThat(comentarios).isNotEmpty();
     }
 
     @Test
-    public void comentariosPersonajeIsNotEmpty(){
+    public void comentariosPersonajeIsNotEmptyTest(){
         Iterable comentarios = comentarioPersonajeRepository.findAll();
         assertThat(comentarios).isNotEmpty();
     }
 
     @Test
-    public void habilidadsIsNotEmpty(){
+    public void habilidadsIsNotEmptyTest(){
         Iterable habilidades = habilidadRepository.findAll();
         assertThat(habilidades).isNotEmpty();
     }
 
     @Test
-    public void itemsIsNotEmpty(){
+    public void itemsIsNotEmptyTest(){
         Iterable items = itemRepository.findAll();
         assertThat(items).isNotEmpty();
     }
 
     @Test
-    public void moderadoresIsNotEmpty(){
+    public void moderadoresIsNotEmptyTest(){
         Iterable moderadores = moderadorRepository.findAll();
         assertThat(moderadores).isNotEmpty();
     }
 
     @Test
-    public void nombresIsNotEmpty(){
+    public void nombresIsNotEmptyTest(){
         Iterable nombres = nombreRepository.findAll();
         assertThat(nombres).isNotEmpty();
     }
 
     @Test
-    public void personajesIsNotEmpty(){
+    public void personajesIsNotEmptyTest(){
         Iterable personajes = personajeRepository.findAll();
         assertThat(personajes).isNotEmpty();
     }
 
     @Test
-    public void ingresoModerador(){
+    public void ingresoModeradorTest(){
         Moderador moderador = moderadorRepository.save(new Moderador(null, "Test", "$2a$12$baupqNti3Y2bOjn7igiOpeS0cE/wXuciqN7P0ZvSp6Y/f051JL.dW"));
         assertThat(moderador).hasFieldOrPropertyWithValue("nombre", "Test");
         assertThat(moderador).hasFieldOrPropertyWithValue("constrasena", "$2a$12$baupqNti3Y2bOjn7igiOpeS0cE/wXuciqN7P0ZvSp6Y/f051JL.dW");
     }
 
     @Test
-    public void ingresoHabilidad(){
+    public void ingresoHabilidadTest(){
         Habilidad habilidad = habilidadRepository.save(new Habilidad(null, "Habilidad", "Esta habilidad es un test", 10L));
         assertThat(habilidad).hasFieldOrPropertyWithValue("nombre", "Habilidad");
         assertThat(habilidad).hasFieldOrPropertyWithValue("descripcion", "Esta habilidad es un test");
@@ -97,7 +97,7 @@ public class JPAUnitTest {
     }
 
     @Test
-    public void ingresoItem(){
+    public void ingresoItemTest(){
         Item item = itemRepository.save(new Item(null, "Item de prueba", "url de prueba", "este es un ítem de prueba"));
         assertThat(item).hasFieldOrPropertyWithValue("nombre", "Item de prueba");
         assertThat(item).hasFieldOrPropertyWithValue("imagen", "url de prueba");
@@ -105,7 +105,7 @@ public class JPAUnitTest {
     }
 
     @Test
-    public void ingresoComentarioItem(){
+    public void ingresoComentarioItemTest(){
         java.sql.Timestamp ts = java.sql.Timestamp.valueOf( "2022-05-21 10:52:49.533000") ;
         ComentarioItem comentarioItem= comentarioItemRepository.save(new ComentarioItem(ts,"comentario de prueba", "usuario de prueba",null));
         comentarioItem.setItem(itemRepository.findById(1L).get());
@@ -114,7 +114,7 @@ public class JPAUnitTest {
     }
 
     @Test
-    public void ingresoComentarioPersonaje(){
+    public void ingresoComentarioPersonajeTest(){
         java.sql.Timestamp ts = java.sql.Timestamp.valueOf( "2022-05-21 10:52:49.533000") ;
         ComentarioPersonaje comentarioPersonaje= comentarioPersonajeRepository.save(new ComentarioPersonaje(ts,"comentario de prueba", "usuario de prueba"));
         comentarioPersonaje.setPersonaje(personajeRepository.findById(1L).get());
